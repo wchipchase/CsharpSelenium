f042ed2c43c77d79a968ed9361375a61ab481e37
Wayne Chipchase <wchipchase@hotmail.com>
2023-08-25 16:47:23 -0500

    Add project files.

---
+++ CsharpFndamentals/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CSharpfndamentals
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
