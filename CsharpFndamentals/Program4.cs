357f91e311ec3f860bd1ff911db6e8e5faf0fd65
Wayne Chipchase <wchipchase@hotmail.com>
2023-08-25 21:33:38 -0500

    Inheritance

---
+++ CsharpFndamentals/Program4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpFndamentals
+{
+    public class Program4
+    {
+        public void setData()
+        {
+            String name;
+            Console.WriteLine("I am in the parent method of program 4");
+        }
+    }
+}
