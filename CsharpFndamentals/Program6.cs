98e5fd10e3c58998a6fd176145db656c8255a991
Wayne Chipchase <wchipchase@hotmail.com>
2023-08-26 15:50:18 -0500

    Lessonseven Arraylist

---
+++ CsharpFndamentals/Program6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+ArrayList a = new ArrayList();               //If you don't know how many items in your collection
+a.Add("Hello");
+a.Add("Bye");
+a.Add("Wayne");
+a.Add("Chipchase");
+
+Console.WriteLine(a[1]);
+
+
+foreach (String item in a)
+{
+    Console.WriteLine(item);
+}
+
+a.Contains("Wayne");
+
+Console.WriteLine(a.Contains("Wayne"));
+
+Console.WriteLine("After Sorting");
+
+a.Sort();
+foreach (String item in a)
+{
+    Console.WriteLine($"{item}");
+}
