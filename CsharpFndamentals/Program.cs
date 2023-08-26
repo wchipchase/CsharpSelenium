bf26652c3a170846a1a09e044cd17657962426ee
Wayne Chipchase <wchipchase@hotmail.com>
2023-08-25 20:45:36 -0500

    Lesson 3

--- CsharpFndamentals/Program.cs
+++ CsharpFndamentals/Program.cs
@@ -8,6 +8,21 @@ namespace CSharpfndamentals
         static void Main(String[] args)
         {
             Console.WriteLine("Hello, World!");
+            int a = 4;
+            Console.WriteLine("Number is " + a);
+
+            String name = "Wayne";
+            Console.WriteLine("Name is " + name);// concatenated string
+
+            Console.WriteLine($"Name is {name}"); //evaluated string
+
+            var age = 51;
+            Console.WriteLine($"Age is {age}"); //var keyword.= don't have to specify data types Use sparingly
+
+            dynamic height = 24.3; //Dynamic data types. Can change a double into a string
+            Console.WriteLine($"Height is {height}");
+            height = "hello";
+            Console.WriteLine($"Height is {height}");
         }
     }
 }
