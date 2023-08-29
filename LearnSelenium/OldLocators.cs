35af6f69e4f10938d6d4b857dd21fcd1f527a3ae
Wayne Chipchase <wchipchase@hotmail.com>
2023-08-29 17:59:59 -0500

    Locators

---
+++ LearnSelenium/OldLocators.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace LearnSelenium
+{
+    public class OldLocators
+    {
+        IWebDriver driver;
+        // Xpath, css, id, classname, name, tagname
+        public void StartBrowser()
+        {
+            //Methods -geturl , click-
+            //Commands go through chromedriver.exe on chrome browser
+            //Specific version of chrome browser
+            //WebDriverManager = compatible chromedriver.exe
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();  //maximizes browser window
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        [Test]
+        public void LocatorIdentification()
+        {
+            driver.FindElement(By.Id("username")).SendKeys("rahulshettyacademy");
+            driver.FindElement(By.Id("username")).Clear();
+        }
+    }
+}
